b818f8831b519f479486377a7fbc65dcbbd1ef07
Oghenefejiro Ephraim Esosuota <ephraim2039@gmail.com>
2022-08-30 22:25:58 +0100

    App POST request working

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
-const router = express.Router()
+const bodyParser = require('body-parser')
+ 
 
-app = express()
+const app = express()
 
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
 app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
+
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    res.json({'status': 'ok'})
+})
 
 app.listen(3000, () => {
     console.log('Server up at 3000')
