ab757b1f75672a180a196bc1f01d1d52e7484494
Oghenefejiro Ephraim Esosuota <ephraim2039@gmail.com>
2022-08-31 19:34:14 +0100

    User creation successful

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
- 
+const User = require('./model/user')
+const bcrypt = require('bcryptjs')
+require('dotenv').config() 
+
 
 const app = express()
+const conn_uri = process.env.MONGO_URI
+
+try{
+    // MongoDB Connection
+    const conn = mongoose.connect(conn_uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    if (conn) console.log('Connection Successful')
+
+} catch(err){
+    console.log(err)
+}
+
 
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
@@ -15,7 +34,20 @@ app.use(bodyParser.json())
 
 
 app.post('/api/register', async (req, res) => {
-    console.log(req.body)
+    
+    var { username, password } = req.body
+    password = await bcrypt.hash(password, 10)
+    
+    try {
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log(`User created successfully. Details: ${response}`)
+    } catch (error) {
+        console.log(error)
+        return res.json({'status': 'error'})
+    }
     res.json({'status': 'ok'})
 })
 
